e2f16681f2ee6059a92d7285343b7176a52578c3
Nesquiko <lukas.castven@gmail.com>
2023-01-03 17:32:53 +0100

    Gorm database struct wrapper

--- pkg/data/base.go
+++ pkg/data/base.go
@@ -1,4 +1,18 @@
+package data
+
+import (
+	"time"
+
+	"github.com/google/uuid"
 	"gorm.io/gorm"
+)
+
+type Base struct {
+	Id        uuid.UUID `gorm:"type:uuid;primaryKey;not null;default:null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
 func (b *Base) BeforeCreate(tx *gorm.DB) error {
 	b.Id = uuid.New()
 	return nil
