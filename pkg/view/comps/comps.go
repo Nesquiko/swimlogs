b5d9f8e75f785522c1d946a30e1ba5591501667e
Nesquiko <lukas.castven@gmail.com>
2023-02-07 21:09:41 +0100

    Training create block form and set form setup

--- pkg/view/comps/comps.go
+++ pkg/view/comps/comps.go
@@ -7,12 +7,16 @@ var startTimeClassError = `bg-red-100 border border-red-300 text-gray-900
 						text-sm rounded-lg focus:ring-red-600
 						focus:border-red-600 block w-full p-2`
 
-var durationClassNormal = `bg-gray-50 border border-gray-300 text-gray-900
+var inputClassNormal = `bg-gray-50 border border-gray-300 text-gray-900
 							text-sm rounded-lg focus:ring-blue-500
 							focus:border-blue-500 block w-full p-2`
-var durationClassError = `bg-red-100 border border-red-300 text-gray-900
+var inputClassError = `bg-red-100 border border-red-300 text-gray-900
 							text-sm rounded-lg focus:ring-red-600
 							focus:border-red-600 block w-full p-2`
+
+var smallInputClassNormal = `bg-gray-50 border border-gray-300 text-gray-900
+							text-sm rounded-lg focus:ring-blue-500
+							focus:border-blue-500 block `
 
 var daySelectClassNormal = `bg-gray-50 border border-gray-300 text-gray-900
 							text-sm rounded-lg focus:ring-blue-500
