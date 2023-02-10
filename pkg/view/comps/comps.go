57dee4eeb0bd2f09301fa943b8177adfc1be9bda
Nesquiko <lukas.castven@gmail.com>
2023-02-10 16:20:16 +0100

    Block name, moving up/down and delete block

--- pkg/view/comps/comps.go
+++ pkg/view/comps/comps.go
@@ -14,10 +14,6 @@ var inputClassError = `bg-red-100 border border-red-300 text-gray-900
 							text-sm rounded-lg focus:ring-red-600
 							focus:border-red-600 block w-full p-2`
 
-var smallInputClassNormal = `bg-gray-50 border border-gray-300 text-gray-900
-							text-sm rounded-lg focus:ring-blue-500
-							focus:border-blue-500 block p-2`
-
 var selectClassNormal = `bg-gray-50 border border-gray-300 text-gray-900
 							text-sm rounded-lg focus:ring-blue-500
 							focus:border-blue-500 block w-full p-2`
