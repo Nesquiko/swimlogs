184134c9a589893b0f9d09f77e2d8402ed0d7938
Nesquiko <lukas.castven@gmail.com>
2023-06-23 23:38:49 +0200

    Create indexes migration

---
+++ backend/migrations/000003_fk_indexes.down.sql
@@ -0,0 +1,2 @@
+drop index if exists blocks_training_id_idx;
+drop index if exists sets_block_id_idx;
---
+++ backend/migrations/000003_fk_indexes.up.sql
@@ -0,0 +1,2 @@
+create index if not exists blocks_training_id_idx on blocks (training_id);
+create index if not exists sets_block_id_idx on sets (block_id);
