b15adee1a12dac0e9efccae495b38fd52b550f7e
Nesquiko <lukas.castven@gmail.com>
2023-06-23 20:06:28 +0200

    Better training preview

--- frontend/src/components/TrainingPreview.tsx
+++ frontend/src/components/TrainingPreview.tsx
@@ -16,37 +16,40 @@ interface TrainingPreviewProps {
 const TrainingPreview: Component<TrainingPreviewProps> = (props) => {
   const block = (block: Block | NewBlock) => {
     return (
-      <div class="flex flex-col p-2">
+      <div class="mx-2">
         <span class="text-2xl">
-          Block {block.num + 1}. {block.name} | <b>{block.totalDistance}m</b>
+          {block.num + 1}. {block.name} | <b>{block.totalDistance}m</b>
         </span>
-        <span class="text-xl">{block.repeat}x</span>
-        <For each={block.sets}>{set}</For>
+        <div class="flex items-center justify-end">
+          <Show when={block.repeat > 1}>
+            <span class="text-3xl font-bold">{block.repeat}x</span>
+          </Show>
+          <div class="w-11/12">
+            <For each={block.sets}>{set}</For>
+          </div>
+        </div>
       </div>
     )
   }
 
   const set = (set: TrainingSet | NewTrainingSet) => {
     return (
-      <div class="mx-8 my-2 flex space-x-4 rounded-lg border border-solid border-slate-300 bg-sky-50 p-2 shadow">
-        <span class="text-lg">Set {set.num + 1}.</span>
-        <div class="flex flex-col">
+      <div class="mx-8 my-2 flex flex-col space-x-4 rounded-lg border border-solid border-slate-300 bg-sky-50 p-2 shadow">
+        <span class="text-lg">
+          {set.repeat} x {set.distance}m
+        </span>
+        <Show when={set.startingRule.type !== StartingRuleType.None}>
           <span class="text-lg">
-            {set.repeat} x {set.distance}m
+            {set.startingRule.type} {set.startingRule.seconds}"
           </span>
-          <span class="whitespace-pre-wrap text-lg">{set.what}</span>
-          <Show when={set.startingRule.type !== StartingRuleType.None}>
-            <span class="text-lg">
-              Start {set.startingRule.type} {set.startingRule.seconds}"
-            </span>
-          </Show>
-        </div>
+        </Show>
+        <span class="whitespace-pre-wrap text-lg">{set.what}</span>
       </div>
     )
   }
 
   return (
-    <div class="m-2">
+    <div class="m-2 space-y-2">
       <h1 class="text-2xl">
         Date{' '}
         <b>
@@ -59,6 +62,9 @@ const TrainingPreview: Component<TrainingPreviewProps> = (props) => {
       </h1>
       <h1 class="text-2xl">
         Start time <b>{props.training?.startTime}</b>
+      </h1>
+      <h1 class="text-2xl">
+        Duration <b>{props.training?.durationMin} min</b>
       </h1>
       <For each={props.training?.blocks}>{block}</For>
       <span class="text-2xl">
