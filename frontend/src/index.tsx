ce6c489ecd45cee8a2648f129ed99e340d83a1f0
Nesquiko <lukas.castven@gmail.com>
2023-06-25 13:07:15 +0200

    Languge detector

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,31 +7,32 @@ import App from './App'
 import { TransProvider } from '@mbarzda/solid-i18next'
 import i18next from 'i18next'
 import I18NextHttpBackend from 'i18next-http-backend'
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
 
 const root = document.getElementById('root')
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?'
-  )
+	throw new Error(
+		'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?'
+	)
 }
 
 render(() => {
-  i18next.use(I18NextHttpBackend)
+	i18next.use(I18NextHttpBackend)
+	i18next.use(I18nextBrowserLanguageDetector)
 
-  const backend = { loadPath: '/locales/{{lng}}/{{ns}}.json' }
+	const backend = { loadPath: '/locales/{{lng}}/{{ns}}.json' }
 
-  return (
-    <TransProvider
-      options={{
-        lng: 'sk',
-        backend,
-        fallbackLng: 'en'
-      }}
-    >
-      <Router>
-        <App />
-      </Router>
-    </TransProvider>
-  )
+	return (
+		<TransProvider
+			options={{
+				backend,
+				fallbackLng: 'en'
+			}}
+		>
+			<Router>
+				<App />
+			</Router>
+		</TransProvider>
+	)
 }, root!)
