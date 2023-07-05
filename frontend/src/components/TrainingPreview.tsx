87cff50232fab2c31b258d6e96b72bbb57532108
Nesquiko <lukas.castven@gmail.com>
2023-07-05 16:43:30 +0200

    fix: training preview

--- frontend/src/components/TrainingPreview.tsx
+++ frontend/src/components/TrainingPreview.tsx
@@ -14,24 +14,6 @@ interface TrainingPreviewProps {
 }
 
 const TrainingPreview: Component<TrainingPreviewProps> = (props) => {
-  /* const block = (block: Block | NewBlock) => { */
-  /*   return ( */
-  /*     <div class="mx-2"> */
-  /*       <span class="text-2xl"> */
-  /*         {block.num + 1}. {block.name} | <b>{block.totalDistance}m</b> */
-  /*       </span> */
-  /*       <div class="flex items-center justify-end"> */
-  /*         <Show when={block.repeat > 1}> */
-  /*           <span class="text-3xl font-bold">{block.repeat}x</span> */
-  /*         </Show> */
-  /*         <div class="w-11/12"> */
-  /*           <For each={block.sets}>{set}</For> */
-  /*         </div> */
-  /*       </div> */
-  /*     </div> */
-  /*   ) */
-  /* } */
-
   const set = (set: TrainingSet | NewTrainingSet) => {
     return (
       <div class="mx-8 my-2 flex flex-col space-x-4 rounded-lg border border-solid border-slate-300 bg-sky-50 p-2 shadow">
@@ -60,7 +42,8 @@ const TrainingPreview: Component<TrainingPreviewProps> = (props) => {
         </p>
         <p class="text-3xl">{formatDate(props.training?.start)}</p>
         <p class="text-3xl">
-          {props.training?.start.toString()} {props.training?.durationMin} min
+          {props.training?.start.toLocaleTimeString()}{' '}
+          {props.training?.durationMin} min
         </p>
       </div>
       <For each={props.training?.sets}>{set}</For>
