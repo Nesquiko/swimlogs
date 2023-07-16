6170e07ff56043a7d326b9f96bbd851a30a186f5
Nesquiko <lukas.castven@gmail.com>
2023-07-16 14:34:24 +0200

    style: update session create page

--- frontend/src/pages/SessionCreatePage.tsx
+++ frontend/src/pages/SessionCreatePage.tsx
@@ -8,24 +8,23 @@ import { SmallIntMax, StartTimeHours, StartTimeMinutes } from '../lib/consts'
 import { sessionApi } from '../state/session'
 
 const SessionCreatePage: Component = () => {
-  const [session, setSession] = createStore<CreateSessionRequest>({
-    day: '' as Day,
-    durationMin: 60,
-    startTime: ''
-  })
-  const [startTime, setStartTime] = createStore({ hours: '06', minutes: '00' })
-  const [isDaySelected, setIsDaySelected] = createSignal(false)
-  const [dayError, setDayError] = createSignal<boolean>(false)
-  const [durationError, setDurationError] = createSignal<boolean>(false)
+  const [day, setDay] = createSignal<Day>(Day.Monday)
+  const [duration, setDuration] = createSignal<number>(60)
+  const [startTime, setStartTime] = createStore<{
+    hours: number
+    minutes: number
+  }>({ hours: 6, minutes: 0 })
 
   const [t] = useTransContext()
   const navigate = useNavigate()
   async function createSession() {
-    const starT = startTime.hours + ':' + startTime.minutes
-    setSession('startTime', starT)
-
-    if (!checkInputErrors()) return
-
+    const session: CreateSessionRequest = {
+      startTime: `${startTime.hours
+        .toString()
+        .padStart(2, '0')}:${startTime.minutes.toString().padStart(2, '0')}`,
+      durationMin: duration(),
+      day: day()
+    }
     await sessionApi
       .createSession({
         createSessionRequest: session
@@ -44,130 +43,103 @@ const SessionCreatePage: Component = () => {
       })
   }
 
-  const checkInputErrors = () => {
-    let isValid = true
-
-    if (!isDaySelected()) {
-      setDayError(true)
-      isValid = false
-    } else {
-      setDayError(false)
-    }
-
-    if (durationError()) {
-      isValid = false
-    }
-
-    return isValid
-  }
-
-  const manualTrainingSessionForm = () => {
-    return (
-      <div class="m-4">
-        <select
-          classList={{
-            'border-red-500': dayError(),
-            'border-slate-300': !dayError()
-          }}
-          class="my-2 w-full rounded-md border border-solid bg-white px-4 py-2 text-xl focus:border-sky-500 focus:outline-none focus:ring"
-          onChange={(e) => {
-            setIsDaySelected(true)
-            setDayError(false)
-            const val = e.target.value
-            setSession('day', val as Day)
-          }}
-        >
-          <option value="" disabled={isDaySelected()}>
-            <Trans key="select.day" />
-          </option>
-          <For each={Object.keys(Day)}>
-            {(day) => (
-              <option value={day}>
-                <Trans key={day.toLowerCase()} />
-              </option>
-            )}
-          </For>
-        </select>
-
-        <div class="flex items-center">
-          <label class="mx-4 w-3/4 text-xl font-bold" for="duration">
+  return (
+    <div>
+      <h1 class="m-4 text-2xl font-bold">
+        <Trans key="create.session" />
+      </h1>
+      <form class="mx-4">
+        <fieldset>
+          <label class="block text-xl" for="day">
+            <Trans key="day" />
+          </label>
+          <select
+            id="day"
+            class="text-cente w-32 rounded-lg border border-solid border-slate-300 bg-white p-2 text-xl focus:border-sky-500 focus:outline-none focus:ring"
+            onChange={(e) => setDay(e.target.value as Day)}
+          >
+            <For each={Object.keys(Day)}>
+              {(d) => (
+                <option selected={d === day()} value={d}>
+                  <Trans key={d.toLowerCase()} />
+                </option>
+              )}
+            </For>
+          </select>
+          <label class="mt-4 block text-xl" for="duration">
             <Trans key="duration.in.minutes" />
           </label>
           <input
             id="duration"
             type="number"
             min="1"
-            placeholder={t('mintes', 'minutes')}
-            classList={{
-              'border-red-500 text-red-500': durationError(),
-              'border-slate-300': !durationError()
-            }}
-            class="w-1/4 rounded-md border px-4 py-2 text-xl focus:border-blue-500 focus:outline-none focus:ring"
-            value={session.durationMin}
+            max={SmallIntMax}
+            class="w-32 rounded-lg border border-solid border-slate-300 bg-white p-2 text-center text-xl focus:border-sky-500 focus:outline-none focus:ring"
+            value={duration()}
             onChange={(e) => {
-              const val = (e.target as HTMLInputElement).value
-              setDurationError(false)
+              const val = e.target.value
               const dur = parseInt(val)
               if (Number.isNaN(dur) || dur < 1 || dur > SmallIntMax) {
-                setDurationError(true)
+                setDuration(60)
                 return
               }
-              setSession('durationMin', dur)
+              setDuration(dur)
             }}
           />
-        </div>
-
-        <div class="flex items-center justify-between">
-          <label class="mx-4 text-xl font-bold">
+          <label class="mt-4 block text-xl" for="hours">
             <Trans key="starttime" />
           </label>
-          <div class="my-2 flex w-auto rounded-md border border-slate-300 px-4 py-2 text-xl">
+          <div class="w-32 rounded-lg border border-slate-300 px-4 py-2 text-center text-xl">
             <select
-              name="hours"
-              class="appearance-none bg-transparent text-xl outline-none"
+              id="hours"
+              class="appearance-none bg-transparent outline-none"
               onChange={(e) => {
                 const val = e.target.value
-                setStartTime('hours', val)
+                const hours = parseInt(val)
+                setStartTime('hours', hours)
               }}
             >
               <For each={Array.from(StartTimeHours)}>
-                {(hour) => <option value={parseInt(hour)}>{hour}</option>}
+                {(hour) => (
+                  <option
+                    selected={parseInt(hour) === startTime.hours}
+                    value={hour}
+                  >
+                    {hour}
+                  </option>
+                )}
               </For>
             </select>
             <span class="mx-2">:</span>
             <select
               name="minutes"
-              class="appearance-none bg-transparent text-xl outline-none"
+              class="appearance-none bg-transparent outline-none"
               onChange={(e) => {
                 const val = e.target.value
-                setStartTime('minutes', val)
+                const minutes = parseInt(val)
+                setStartTime('minutes', minutes)
               }}
             >
               <For each={Array.from(StartTimeMinutes)}>
-                {(minute) => <option value={parseInt(minute)}>{minute}</option>}
+                {(minute) => (
+                  <option
+                    selected={parseInt(minute) === startTime.minutes}
+                    value={minute}
+                  >
+                    {minute}
+                  </option>
+                )}
               </For>
             </select>
           </div>
-        </div>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <h1 class="m-4 text-2xl font-bold">
+        </fieldset>
+      </form>
+      <button
+        class="m-4 rounded-lg bg-sky-500 p-4 text-xl font-bold text-white"
+        onClick={createSession}
+      >
         <Trans key="create.session" />
-      </h1>
-
-      {manualTrainingSessionForm()}
-
-      <div class="text-center">
-        <button
-          class="mx-auto rounded bg-green-600 p-4 text-xl font-bold text-white"
-          onClick={createSession}
-        >
-          <Trans key="create.session" />
-        </button>
-      </div>
+      </button>
     </div>
   )
 }
