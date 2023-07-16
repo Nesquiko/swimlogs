c78dda8d85ab129c4c6ed1a07d95f9b848e0f2fd
Nesquiko <lukas.castven@gmail.com>
2023-07-16 16:02:41 +0200

    feat: set start time input in minutes and seconds

--- frontend/src/components/SetModal.tsx
+++ frontend/src/components/SetModal.tsx
@@ -1,6 +1,5 @@
 import { Trans, useTransContext } from '@mbarzda/solid-i18next'
-import { document } from 'postcss'
-import { createEffect, For, JSX, on, onMount } from 'solid-js'
+import { createEffect, createSignal, For, JSX, on, onMount } from 'solid-js'
 import { createStore, reconcile } from 'solid-js/store'
 import { NewTrainingSet, StartType } from '../generated'
 import { SmallIntMax } from '../lib/consts'
@@ -14,11 +13,29 @@ type SetModalProps = {
 
 function SetModal(props: SetModalProps): JSX.Element {
   let dialog: HTMLDialogElement
+  const [startTimeSeconds, setStartTimeSeconds] = createSignal(0)
+  const [startTimeSecondsErr, setStartTimeSecondsErr] = createSignal(false)
+
+  const [startMinutes, setStartMinutes] = createSignal(0)
+  const [startMinutesErr, setStartMinutesErr] = createSignal(false)
 
   createEffect(
     on(
       () => props.opener,
-      () => setTrainingSet(reconcile(props.opener.set)),
+      () => {
+        setStartMinutes(0)
+        setStartTimeSeconds(0)
+        setStartMinutesErr(false)
+        setStartTimeSecondsErr(false)
+
+        if (props.opener.set.startType !== StartType.None) {
+          const seconds = props.opener.set.startSeconds! % 60
+          const minutes = (props.opener.set.startSeconds! - seconds) / 60
+          setStartMinutes(minutes)
+          setStartTimeSeconds(seconds)
+        }
+        setTrainingSet(reconcile(props.opener.set))
+      },
       { defer: true }
     )
   )
@@ -58,31 +75,59 @@ function SetModal(props: SetModalProps): JSX.Element {
     })
   })
 
+  const submit = () => {
+    if (!isSetValid()) return
+
+    // this modal only creates sets without subsets, so we can just shallow copy the object
+    const ts = Object.assign({}, trainingSet)
+    if (ts.startType !== StartType.None) {
+      ts.startSeconds = startTimeSeconds() + startMinutes() * 60
+    } else {
+      ts.startSeconds = undefined
+    }
+    props.onSubmitSet(ts, props.opener.idx)
+    dialog.close()
+  }
+
   const isSetValid = (): boolean => {
+    let isValid = true
     if (trainingSet.repeat < 1) {
-      return false
+      isValid = false
     }
 
     if (
       trainingSet.distanceMeters === undefined ||
       trainingSet.distanceMeters < 1
     ) {
-      return false
+      isValid = false
     }
 
-    if (
-      trainingSet.startType !== StartType.None &&
-      trainingSet.startSeconds !== undefined &&
-      trainingSet.startSeconds < 1
-    ) {
-      return false
+    if (trainingSet.startType !== StartType.None) {
+      if (startTimeSeconds() < 0 || startTimeSeconds() > 59) {
+        setStartTimeSecondsErr(true)
+        isValid = false
+      }
+
+      if (startMinutes() < 0 || startMinutes() > 59) {
+        setStartMinutesErr(true)
+        isValid = false
+      }
+
+      if (startMinutes() * 60 + startTimeSeconds() === 0) {
+        setStartMinutesErr(true)
+        setStartTimeSecondsErr(true)
+        isValid = false
+      }
     }
 
-    return true
+    return isValid
   }
 
   return (
-    <dialog ref={dialog!} class="h-screen w-11/12 rounded-lg">
+    <dialog
+      ref={dialog!}
+      class="h-screen w-11/12 rounded-lg md:w-5/6 lg:w-2/3 xl:w-1/3"
+    >
       <p class="text-center text-2xl">
         <Trans key="add.new.set" />
       </p>
@@ -99,7 +144,7 @@ function SetModal(props: SetModalProps): JSX.Element {
             'border-red-500 text-red-500': trainingSet.repeat < 1,
             'border-slate-300': trainingSet.repeat >= 1
           }}
-          class="w-24 rounded-md border p-2 text-center text-lg focus:border-blue-500 focus:outline-none focus:ring"
+          class="w-24 rounded-lg border p-2 text-center text-lg focus:border-blue-500 focus:outline-none focus:ring"
           value={trainingSet.repeat}
           onChange={(e) => {
             let repeat = parseInt(e.target.value)
@@ -130,7 +175,7 @@ function SetModal(props: SetModalProps): JSX.Element {
               trainingSet.distanceMeters === undefined ||
               trainingSet.distanceMeters >= 1
           }}
-          class="w-24 rounded-md border p-2 text-center text-lg focus:border-blue-500 focus:outline-none focus:ring"
+          class="w-24 rounded-lg border p-2 text-center text-lg focus:border-blue-500 focus:outline-none focus:ring"
           value={trainingSet.distanceMeters ?? '0'}
           onChange={(e) => {
             let dist = parseInt(e.target.value)
@@ -148,7 +193,7 @@ function SetModal(props: SetModalProps): JSX.Element {
         </label>
         <select
           id="start"
-          class="w-32 rounded-md border border-solid border-slate-300 bg-white p-2 text-start text-lg focus:border-sky-500 focus:outline-none focus:ring"
+          class="w-32 rounded-lg border border-solid border-slate-300 bg-white p-2 text-start text-lg focus:border-sky-500 focus:outline-none focus:ring"
           onChange={(e) => {
             setTrainingSet('startType', e.target.value as StartType)
           }}
@@ -167,42 +212,52 @@ function SetModal(props: SetModalProps): JSX.Element {
           visible: trainingSet.startType !== StartType.None,
           invisible: trainingSet.startType === StartType.None
         }}
-        class="my-2 flex items-center justify-between"
+        class="my-2 text-end"
       >
-        <label class="text-xl" for="seconds">
-          <Trans key="seconds" />
-        </label>
+        <input
+          id="minutes"
+          type="number"
+          placeholder={t('minutes', 'minutes')}
+          value={startMinutes() === 0 ? '' : startMinutes()}
+          min="0"
+          max="59"
+          classList={{
+            'border-red-500 text-red-500': startMinutesErr(),
+            'border-slate-300': !startMinutesErr()
+          }}
+          class="mx-2 w-24 rounded-lg border border-solid bg-white p-2 text-center text-lg focus:border-sky-500 focus:outline-none focus:ring"
+          onChange={(e) => {
+            const val = e.target.value
+            let minutes = parseInt(val)
+            setStartMinutesErr(false)
+            if (Number.isNaN(minutes) || minutes < 0 || minutes > SmallIntMax) {
+              minutes = 0
+            }
+            setStartMinutes(minutes)
+          }}
+        />
         <input
           id="seconds"
           type="number"
-          placeholder="20"
+          value={startTimeSeconds() === 0 ? '' : startTimeSeconds()}
+          min="0"
+          max="59"
+          placeholder={t('seconds', 'seconds')}
           classList={{
-            'border-red-500 text-red-500':
-              trainingSet.startSeconds !== undefined &&
-              trainingSet.startSeconds < 1,
-            'border-slate-300':
-              trainingSet.startSeconds === undefined ||
-              trainingSet.startSeconds >= 1
+            'border-red-500 text-red-500': startTimeSecondsErr(),
+            'border-slate-300': !startTimeSecondsErr()
           }}
-          class="w-24 rounded-md border border-solid border-slate-300 bg-white p-2 text-center text-lg focus:border-sky-500 focus:outline-none focus:ring"
-          value={trainingSet.startSeconds ?? ''}
+          class="w-24 rounded-lg border border-solid bg-white p-2 text-center text-lg focus:border-sky-500 focus:outline-none focus:ring"
           onChange={(e) => {
             const val = e.target.value
             let seconds = parseInt(val)
+            setStartTimeSecondsErr(false)
             if (Number.isNaN(seconds) || seconds < 1 || seconds > SmallIntMax) {
               seconds = 0
             }
-            setTrainingSet('startSeconds', seconds)
+            setStartTimeSeconds(seconds)
           }}
-        >
-          <For each={Object.keys(StartType)}>
-            {(typ) => (
-              <option selected={typ === trainingSet.startType} value={typ}>
-                <Trans key={typ.toLowerCase()} />
-              </option>
-            )}
-          </For>
-        </input>
+        />
       </div>
       <label class="text-xl" for="description">
         <Trans key="description" />
@@ -233,12 +288,7 @@ function SetModal(props: SetModalProps): JSX.Element {
         </button>
         <button
           class="rounded-lg bg-sky-500 px-4 py-2 font-bold text-white hover:bg-sky-600 focus:outline-none focus:ring focus:ring-sky-300"
-          onClick={() => {
-            if (!isSetValid()) return
-            // this modal only creates sets without subsets, so we can just shallow copy the object
-            props.onSubmitSet(Object.assign({}, trainingSet), props.opener.idx)
-            dialog.close()
-          }}
+          onClick={submit}
         >
           {submitLabel}
         </button>
