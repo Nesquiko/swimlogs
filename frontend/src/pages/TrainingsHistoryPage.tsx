01cbb77553fa41cfb88592ee2cfdf71974e82cc9
Nesquiko <lukas.castven@gmail.com>
2023-07-27 18:05:31 +0200

    feat: display error message when server error in training history

--- frontend/src/pages/TrainingsHistoryPage.tsx
+++ frontend/src/pages/TrainingsHistoryPage.tsx
@@ -1,6 +1,6 @@
 import { Trans } from '@mbarzda/solid-i18next'
 import { useNavigate } from '@solidjs/router'
-import { Component, createResource, createSignal, For } from 'solid-js'
+import { Component, createResource, createSignal, For, Show } from 'solid-js'
 import DetailCard from '../components/DetailCard'
 import Pagination from '../components/Pagination'
 import { ResponseError, TrainingDetail } from '../generated'
@@ -13,12 +13,14 @@ const TrainingHistoryPage: Component = () => {
 
   const [detailsPage, setDetailsPage] = createSignal(0)
   const [totalDetails, setTotalDetails] = createSignal(0)
+  const [serverError, setServerError] = createSignal(false)
   const isLastPage = () => (detailsPage() + 1) * PAGE_SIZE >= totalDetails()
 
   const cachedTrainingDetails = new Map<number, TrainingDetail[]>()
   const [details] = createResource(detailsPage, getTrainingDetals)
   async function getTrainingDetals(page: number): Promise<TrainingDetail[]> {
     if (cachedTrainingDetails.has(page)) {
+      setServerError(false)
       return Promise.resolve(cachedTrainingDetails.get(page)!)
     }
 
@@ -26,11 +28,13 @@ const TrainingHistoryPage: Component = () => {
       .getTrainingsDetails({ page, pageSize: PAGE_SIZE })
       .then((res) => {
         setTotalDetails(res.pagination.total)
+        setServerError(false)
         cachedTrainingDetails.set(page, res.details)
         return res.details
       })
       .catch((e: ResponseError) => {
         console.error('error', e)
+        setServerError(true)
         return Promise.resolve([])
       })
   }
@@ -41,6 +45,11 @@ const TrainingHistoryPage: Component = () => {
         <Trans key="trainings.history" />
       </h1>
       <div class="h-5/6">
+        <Show when={serverError()}>
+          <div class="m-4 flex items-center justify-start rounded bg-red-300 p-4 font-bold">
+            <Trans key="couldnt.load.trainings" />
+          </div>
+        </Show>
         <For each={details()}>
           {(detail) => (
             <div onClick={() => navigate('/training/' + detail.id)}>
@@ -52,7 +61,7 @@ const TrainingHistoryPage: Component = () => {
       <Pagination
         prevDisabled={detailsPage() === 0}
         onPrevPage={() => setDetailsPage((i) => i - 1)}
-        nextDisabled={isLastPage()}
+        nextDisabled={isLastPage() || serverError()}
         onNextPage={() => setDetailsPage((i) => i + 1)}
       />
     </div>
