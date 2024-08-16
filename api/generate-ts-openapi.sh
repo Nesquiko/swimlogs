d41a59da581c9a5d7bd454b2bcedc5a65ebd9c66
Nesquiko <lukas.castven@gmail.com>
2024-08-16 19:27:10 +0200

    feat: define api for set components and new set fields

--- api/generate-ts-openapi.sh
+++ api/generate-ts-openapi.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 openapi-generator-cli generate -g typescript-fetch -i ./swimlogsAPI.gen.yaml \
-	--inline-schema-name-mappings summariesCurrentWeek_200_response=SummariesCurrentWeekResponse \
-	--inline-schema-name-mappings summariesPage_200_response=SummariesPageResponse \
-	--inline-schema-name-mappings moveSet_request=MoveSetRequest \
+	--inline-schema-name-mappings replaceSetComponents_request=ReplaceSetComponentsRequest \
 	--inline-schema-name-mappings editSet_request=EditSetRequest \
-	--inline-schema-name-mappings editSet_200_response=EditSetResponse \
-	--inline-schema-name-mappings trainingById_404_response=TrainingNotFoundResponse \
-	--inline-schema-name-mappings createTraining_400_response=InvalidTrainingResponse
+	--inline-schema-name-mappings moveSet_request=MoveSetRequest \
+	--inline-schema-name-mappings summariesPage_200_response=SummariesPageResponse
